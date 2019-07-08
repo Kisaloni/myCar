ed693f54a6f5c646fe447bd6d5bceb7a835e9bfb
kishan Dasondhi <kishandasondhi123@gmail.com>
2019-07-08 20:35:09 +0530

    Update CAR.ino

--- CAR/CAR.ino
+++ CAR/CAR.ino
@@ -1,13 +1,13 @@
 #include<Servo.h>
-
+#include <ESP8266WiFi.h>
 /**changes starts */
-IPAddress staticIP832_150(192,168,1,150);
-IPAddress gateway832_150(192,168,1,1);
+IPAddress staticIP832_150(192,168,79,15);
+IPAddress gateway832_150(192,168,79,1);
 IPAddress subnet832_150(255,255,255,0);
+/**changes ends */
 
 String  i;
 WiFiServer server(80);
-/**changes ends */
 
 #define headLight 16
 #define breakLight 5
@@ -39,7 +39,7 @@ void setup() {
   digitalWrite(mot2For,LOW);
   digitalWrite(mot2Back,LOW);
   Serial.begin(9600);
-  /**changes starts */
+ 
   i = "";
   WiFi.disconnect();
   delay(1000);
@@ -51,8 +51,10 @@ void setup() {
     delay(300);
     Serial.print("...");
   }
-  delay(1000);
+  delay(1000); 
+  /**changes starts */
   WiFi.config(staticIP832_150, gateway832_150, subnet832_150);
+  /**changes ends */
   delay(1000);
   Serial.println("Connected to dlink");
   Serial.println("Local IP:");
@@ -61,11 +63,12 @@ void setup() {
   Serial.println((WiFi.gatewayIP().toString().c_str()));
   Serial.println("Host Name");
   Serial.println((WiFi.hostname()));
-  /**changes ends */
+  server.begin();
+  Serial.println("Goto: http://"+(WiFi.localIP().toString())+"/");
+  
 }
 
 void loop() {
-    /**changes starts */
     WiFiClient client = server.available();
     if (!client) { return; }
     while(!client.available()){  delay(1); }
@@ -73,12 +76,11 @@ void loop() {
     i.remove(0, 5);
     i.remove(i.length()-9,9);
     Serial.println(i);
-    command=parseInt(i);
-    /**changes ends */
-    // while(Serial.available()==0){
-    //   //Serial.println("...");  
-    // }int command=0;
-    // command=Serial.parseInt();
+    int command=i.toInt();
+//     while(Serial.available()==0){
+//       //Serial.println("...");  
+//     }int command=0;
+//     command=Serial.parseInt();
     Serial.println(command);
   switch(command){
     case 1:
@@ -92,44 +94,65 @@ void loop() {
       delay(1000);
       break;
     case 3:
-      Serial.println("BreakLight on");
-      digitalWrite(breakLight,HIGH);
-      delay(1);
-      break;
-    case 4:
-      Serial.println("BreakLight off");
-      digitalWrite(breakLight,LOW);
-      delay(1);
-      break;
-    case 5:
       Serial.println("Moving forward");
       digitalWrite(mot1For,HIGH);delay(1);
       digitalWrite(mot1Back,LOW);delay(1);
       digitalWrite(mot2For,HIGH);delay(1);
       digitalWrite(mot2Back,LOW);delay(1);
       break;
-    case 6:
+    case 4:
       Serial.println("Moving backward");
       digitalWrite(mot1For,LOW);delay(1);
       digitalWrite(mot1Back,HIGH);delay(1);
       digitalWrite(mot2For,LOW);delay(1);
       digitalWrite(mot2Back,HIGH);delay(1);
       break;
-    case 7:
+    case 5:
       Serial.println("Stoping car");
       digitalWrite(mot1For,LOW);delay(1);
       digitalWrite(mot1Back,LOW);delay(1);
       digitalWrite(mot2For,LOW);delay(1);
       digitalWrite(mot2Back,LOW);delay(1);
+      Serial.println("BreakLight on");
+      digitalWrite(breakLight,HIGH);
+      delay(1000);
+      Serial.println("BreakLight off");
+      digitalWrite(breakLight,LOW);
+      delay(1);
       break;
-    case 9:
+    case 6:
       Serial.println("Turning left");
       steer.write(65);
+      delay(2000);
+      steer.write(90);
       break;
-    case 10:
+    case 7:
       Serial.println("Turning right");
       steer.write(115);
+      delay(2000);
+      steer.write(90);
       break;
-     default:digitalWrite(headLight,LOW);break;
+     case 8:
+      setup();
+    default:
+      digitalWrite(headLight,LOW);
+      digitalWrite(breakLight,LOW);
+      digitalWrite(mot1En,HIGH);
+      digitalWrite(mot1For,LOW);
+      digitalWrite(mot1Back,LOW);
+      digitalWrite(mot2En,HIGH);
+      digitalWrite(mot2For,LOW);
+      digitalWrite(mot2Back,LOW);
+      break;
   }
+  digitalWrite(4,HIGH);
+  digitalWrite(D4,HIGH);
+  client.println("HTTP/1.1 200 OK");
+  client.println("Content-Type: text/html");
+  client.println("");
+  client.println("<!DOCTYPE HTML>");
+  client.println("<html>");
+  client.println("LED ON");
+  client.println("</html>");
+  delay(1);
 }
